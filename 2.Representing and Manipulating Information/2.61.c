55da8d11d1e11e37a2204c9dee98eff20e5365ad
A22H67 <76444803+A22H67@users.noreply.github.com>
2021-03-14 23:37:57 +0700

    Create 2.61.c

---
+++ 2.Representing and Manipulating Information/2.61.c
@@ -0,0 +1,27 @@
+/*
+Write C expressions that evaluate to 1 when the following conditions are true and
+to 0 when they are false. Assume x is of type int.
+A. Any bit of x equals 1.
+B. Any bit of x equals 0.
+C. Any bit in the least significant byte of x equals 1.
+D. Any bit in the most significant byte of x equals 0.
+*/
+#include<stdio.h>
+
+int A(int x){
+    return x && 1;
+}
+
+int B(int x){
+    return !(~x==0);
+}
+
+int C(int x){
+    return (x &0xff) && 1;
+}
+
+int D(int x){
+    int shift_val=(sizeof(int)-1)<<3;
+    int x_most=x >> shift_val;
+    return!(~x_most==0);
+}
