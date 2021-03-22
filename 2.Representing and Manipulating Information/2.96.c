5eca2ed33b6468f3524ebe17d265e7c709725683
A22H67 <76444803+A22H67@users.noreply.github.com>
2021-03-22 21:56:45 +0700

    Update 2.96.c

--- 2.Representing and Manipulating Information/2.96.c
+++ 2.Representing and Manipulating Information/2.96.c
@@ -1,5 +1,13 @@
 /*
+Following the bit-level floating-point coding rules, implement the function with
+the following prototype:
 
+* Compute (int) f.
+* If conversion causes overflow or f is NaN, return 0x80000000
+
+int float_f2i(float_bits f);For floating-point number f , this function computes (int) f . Your function
+should round toward zero. If f cannot be represented as an integer (e.g., it is out
+of range, or it is NaN), then the function should return 0x80000000
 */
 #include <stdio.h>
 typedef unsigned float_bits;
