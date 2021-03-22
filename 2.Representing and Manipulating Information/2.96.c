a0ef177d6dac28febd69f504ba55df09eed2abb7
A22H67 <76444803+A22H67@users.noreply.github.com>
2021-03-22 22:07:40 +0700

    Update 2.96.c

--- 2.Representing and Manipulating Information/2.96.c
+++ 2.Representing and Manipulating Information/2.96.c
@@ -11,25 +11,33 @@ of range, or it is NaN), then the function should return 0x80000000
 */
 #include <stdio.h>
 typedef unsigned float_bits;
-float_bits float_f2i(float_bits f){
+float_bits float_f2i(float_bits f)
+{
     unsigned exp=(f>>23) & 0xff ;
     unsigned frac=f&0x7fffff;
     unsigned s=f>>31;
     unsigned bias=127;
-    if(exp==0xff && frac!=0){
-        return 0x80000000;
+    if(exp==0xff && frac!=0)
+    {
+        return 0x80000000;//NAN
     }
-
     exp=exp-127;
-    unsigned k=23-exp;
+    unsigned k;
 
-    if(exp>32){
-        return 0x80000000;
-    }else if(exp>23){
-        k=exp-23;
+    if(exp>32)
+    {
+        return 0x80000000;//overflow
     }
-    
-    frac=frac >> k;
+    else if(exp>23)
+    {
+        k=exp-23;
+        frac=frac << k;
+    }
+    else
+    {
+        k=23-exp;
+        frac=frac >> k;
+    }
     return (1<<(exp))|frac;
 }
 int main()
@@ -37,7 +45,6 @@ int main()
     unsigned x=0x4A564504;
     printf("0x%x convert ",x);
     printf("0x%x\n",float_f2i(x));
-
     x=0x4640E400;
     printf("0x%x convert ",x);
     printf("0x%x\n",float_f2i(x));
