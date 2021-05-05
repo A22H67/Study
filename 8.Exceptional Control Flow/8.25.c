8c458835927c53c2d5be3e8479070e186b5b7c6c
A22H67 <76444803+A22H67@users.noreply.github.com>
2021-05-05 13:57:47 +0700

    Update 8.25.c

--- 8.Exceptional Control Flow/8.25.c
+++ 8.Exceptional Control Flow/8.25.c
@@ -1,3 +1,11 @@
+/*
+Write a version of the fgets function, called tfgets, that times out after 5 seconds.
+The tfgets function accepts the same inputs as fgets. If the user doesn’t type an
+input line within 5 seconds, tfgets returns NULL. Otherwise, it returns a pointer
+to the input line.
+
+*/
+
 #include "csapp.h"
 #include <termios.h>
 #include <unistd.h>
