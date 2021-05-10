7a75eb02779586bb208ffb86c9e6d9aa944e0db9
A22H67 <76444803+A22H67@users.noreply.github.com>
2021-05-10 17:21:18 +0700

    Update myshell.c

--- 8.Exceptional Control Flow/myshell.c
+++ 8.Exceptional Control Flow/myshell.c
@@ -1,3 +1,34 @@
+/*
+Using the example in Figure 8.23 as a starting point, write a shell program that
+supports job control. Your shell should have the following features:
+.
+The command line typed by the user consists of a name and zero or more argu-
+ments, all separated by one or more spaces. If name is a built-in command, the
+shell handles it immediately and waits for the next command line. Otherwise,
+the shell assumes that name is an executable file, which it loads and runs in the
+context of an initial child process (job). The process group ID for the job is
+identical to the PID of the child.
+
++Each job is identified by either a process ID (PID) or a job ID (JID), which
+is a small arbitrary positive integer assigned by the shell. JIDs are denoted on
+the command line by the prefix ‘%’. For example, ‘%5’ denotes JID 5, and ‘5’
+denotes PID 5.
++If the command line ends with an ampersand, then the shell runs the job in
+the background. Otherwise, the shell runs the job in the foreground.
++Typing Ctrl+C (Ctrl+Z) causes the kernel to send a SIGINT (SIGTSTP) signal
+to your shell, which then forwards it to every process in the foreground process
+group
++The jobs built-in command lists all background jobs.
++The bg job built-in command restarts job by sending it a SIGCONT signal
+and then runs it in the background. The job argument can be either a PID or
+a JID.
++The fg job built-in command restarts job by sending it a SIGCONT signal and
+then runs it in the foreground.
++The shell reaps all of its zombie children. If any job terminates because it
+receives a signal that was not caught, then the shell prints a message to the
+terminal with the job’s PID and a description of the offending signal.
+
+*/
 #include "csapp.h"
 #define MAXARGS 128
 
@@ -13,7 +44,6 @@ void deleteJob(int index){
         listid[index]=listid[index+1];
     }
     jobs--;
-  //  bgjob[jobs]=NULL;
 }
 void addjob(int pid,char *cmdline){
     char *p=malloc(strlen(cmdline));
@@ -69,7 +99,6 @@ void eval(char *cmdline)
         {
             //back ground
             addjob(pid,cmdline);
-          //  kill(pid,SIGSTOP);
             printf("PID:%d-%s\n",pid,cmdline);
         }
 
@@ -138,18 +167,7 @@ int builtin_command(char **argv)
             while(i<=jobs)
             {
 
-                waitpid(listid[i],&status,WNOHANG);
-                if(WIFEXITED(status)) //=========================================Here===========================================================================================================
-                {
-                    printf("[%d] %d stop %s\n",i,listid[i],bgjob[i]);
-                }
-                else
-                {
-                    printf("[%d] %d running %s\n",i,listid[i],bgjob[i]);
-
-                }//=============================================================Here=======================================================================
-
-
+                printf("[%d] %d stop %s\n",i,listid[i],bgjob[i]);
                 i++;
             }
 
@@ -243,9 +261,7 @@ int parseJob(char *job_n){
             int flag=0;
             int inde;
             for( inde=0;inde<=jobs;inde++){
-                //printf("S:%d\n",listid[inde]);
                 if(listid[inde]==i){
-                    //printf("PID:%d",listid[inde]);
                     flag=1;
                     break;
                 }
@@ -261,7 +277,6 @@ int parseJob(char *job_n){
 
 }
 void handlerCon(){
-  //  printf("PID:%d\n",listid[indexx]);
   sigset_t mask,prev_mask;
   sigfillset(&mask);
   sigprocmask(SIG_BLOCK,&mask,&prev_mask);
@@ -274,11 +289,8 @@ void handlerCon(){
 
 int main()
 {
-    // jobs ctrl+c ctrl+z bg fg
     char cmdline[MAXLINE];
-
     signal(SIGCONT,handlerCon);
-
     while(1)
     {
         printf("> ");
