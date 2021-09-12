dd2a067a01345d08e32e7c3e0b9771546fc0bde2
A22H67 <76444803+A22H67@users.noreply.github.com>
2021-09-13 00:05:03 +0700

    Update and rename ls.cpp to scanDirectory.cpp

---
+++ example/scanDirectory.cpp
@@ -0,0 +1,41 @@
+/*
+ Program that scans all directories in a UNIX file system
+*/
+
+#include <dirent.h>
+#include <errno.h>
+#include<stdio.h>
+#include <sys/stat.h>
+#include <string.h>
+void read_d(char *filename){
+    DIR *dir;
+    struct dirent *entry;
+
+    if ((dir = opendir(filename)) == NULL) return;
+    else
+    {
+        printf("contents of %s:\n",filename);
+        while((entry=readdir(dir)) !=NULL )
+        {
+
+            struct stat st;
+            stat(entry->d_name,&st);
+            char *file=entry->d_name;
+            //skip if filename is current or parent directory(prevent loop)
+            if(strcmp(file,".")==0) continue; 
+            if(strcmp(file,"..")==0) continue;
+            
+            printf("%s:%d bytes mode:%d \n\n",file,st.st_size,st.st_mode);
+            read_d(entry->d_name);
+        }//end while
+
+    }//end else
+    printf("end content of %s\n\n",filename);
+    closedir(dir);
+
+}
+main(int argc,char *argv[])
+{
+   read_d("/home/tr/Desktop");
+
+}
